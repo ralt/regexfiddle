4e016349ecd9865d8017135ba6801e4b297283f4
Florian Margaine <florian@margaine.com>
2012-11-16 13:27:51 +0100

    fix

--- script.js
+++ script.js
@@ -3,23 +3,23 @@
     /*jshint browser:true */
     'use strict';
 
-    document.addEventListener('DOMContentLoaded', function() {
-        var input = document.getElementById('input'),
-            output = document.getElementById('output');
+    var input = document.getElementById('input'),
+        output = document.getElementById('output');
 
-        input.addEventListener('input', regex, false);
+    input.addEventListener('input', regex, false);
 
-        function regex() {
-            var match = output.textContent.match(new RegExp(input.value, 'g'));
+    function regex() {
+        var match = output.textContent.match(new RegExp(input.value, 'g'));
 
-            // Reset the text
-            output.innerHTML = output.textContent;
+        // Reset the text
+        output.innerHTML = output.textContent;
 
+        if (match) {
             // Replace!
             match.forEach(function(el) {
                 output.innerHTML = output.innerHTML.replace(new RegExp(el, 'g'), '<span class="highlighted">' + el + '</span>');
             });
         }
-    }, false);
+    }
 
 }());
