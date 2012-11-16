4d3d7bcda8062ec80c255769f04c6b9316e94bea
Florian Margaine <florian@margaine.com>
2012-11-16 13:10:55 +0100

    Removes the keyup on output.

--- script.js
+++ script.js
@@ -8,7 +8,6 @@
         output = document.getElementById('output');
 
         input.addEventListener('keyup', regex, false);
-        output.addEventListener('keyup', regex, false);
 
         function regex() {
             var match = output.textContent.match(new RegExp(input.value, 'g'));
@@ -20,14 +19,6 @@
             match.forEach(function(el) {
                 output.innerHTML = output.innerHTML.replace(new RegExp(el, 'g'), '<span class="highlighted">' + el + '</span>');
             });
-
-            // Move the cursor to the end
-            var range = document.createRange();
-            range.selectNodeContents(output);
-            range.collapse(false);
-            var selection = window.getSelection();
-            selection.removeAllRanges();
-            selection.addRange(range);
         }
     }, false);
 
